00b3dcafc0c84f7e9ff53801c34646a9765a701a
awalemandi <mandi.awale@gmail.com>
2021-03-30 16:08:49 +1100

    created getRegexMatch function

--- src/Regex.js
+++ src/Regex.js
@@ -1,12 +1,13 @@
-
 export const portRegex = /^[0-9]+-[0-9]+$/i;
 
-//function to remove ports from input and return patch panel
-const getPatchpanel = input => {
-  //SY4:01:010105:0309:RU42R:001-048
-  //regex  = colon+numberx3 dash numberx3
-  //input.replace(ports match with ':')
-  //return patch panel string
+
+//function to return the regex match as string
+export const getRegexMatch = (regex, inputString) => {
+  if (regex.test(inputString)) {
+    let matchArray = inputString.match(regex);
+    return matchArray[0];
+  } else {
+    console.log(`No regex match found!`);
+    return;
+  }
 };
-
-
