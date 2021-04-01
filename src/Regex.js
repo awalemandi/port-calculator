ca478175525e68e7cc4f1dbfa3d1d10158c43e9a
awalemandi <mandi.awale@gmail.com>
2021-04-01 16:35:52 +1100

    defined regex pattern for range separator and function to get A and Z sides

--- src/Regex.js
+++ src/Regex.js
@@ -1,7 +1,19 @@
+export const rangeSeparatorRegex = / to /gim;
+
+//fucntion to return array of A and Z sides from a range(string)
+const getSides = (regex, range) => {
+  if (regex.test(range)) {
+    let sidesArray = range.split(regex);
+    return sidesArray;
+  } else {
+    console.log(`Range template invalid!`);
+    return;
+  }
+};
+
 export const portRangeRegex = /([0-9]+-[0-9]+)$/i;
 
-//for patch panel, replace portRange with '' in 
-
+//for patch panel, replace portRange with '' in
 
 //function to return the regex match as string
 export const getRegexMatch = (regex, inputString) => {
@@ -12,4 +24,12 @@ export const getRegexMatch = (regex, inputString) => {
     console.log(`No regex match found!`);
     return;
   }
+};
+
+//function to get A and Z sides
+export const getAside = range => {
+  //return A side
+};
+export const getZside = range => {
+  //return Z side
 };
